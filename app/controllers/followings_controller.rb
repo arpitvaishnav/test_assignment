2d609ce59a1b7fe5ff7b1e26ab77dbe5c1b895ef
Arpit <arpit.vaishnav@atharvasytem.com>
2020-03-06 17:36:40 +0530

    added followings and followers

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -3,9 +3,27 @@ class FollowingsController < ApplicationController
 
 
   def follow_user
-    
+    user = User.find_by_id(params[:id])
+    @followings = current_user.followeds.new(followable_type: 'User', followable_id: user.id, follower_id: user.id)
+  
+    respond_to do |format|
+      if @followings.save
+        format.html { redirect_to root_path, notice: 'Followed Successfully' }
+      else
+        format.html { redirect_to root_path, notice: "Could not Follow : Reasons #{@followings.errors.full_messages.join(', ')}" }
+      end
+    end
   end
 
   def follow_topic
+    topic = Topic.find_by_id(params[:id])
+    @followings = current_user.followeds.new(followable_type: 'Topic', followable_id: topic.id, follower_id: topic.user_id)
+    respond_to do |format|
+      if @followings.save
+        format.html { redirect_to root_path, notice: 'Followed Successfully' }
+      else
+        format.html { redirect_to root_path, notice: "Could not Follow : Reasons #{@followings.errors.full_messages.join(', ')}" }
+      end
+    end
   end
 end
--- app/models/following.rb
+++ app/models/following.rb
@@ -1,3 +1,13 @@
 class Following < ApplicationRecord
   belongs_to :followable, :polymorphic => true
+
+  validate :same_user_cant_follow_his_own_items
+  private def same_user_cant_follow_his_own_items
+    case followable_type
+    when 'User'
+      self.errors[:base] <<  'User can not follow him self' if followed_id.eql?(followable_id) 
+    when 'Topic'
+      self.errors[:base] <<  'User can not follow his own topic' if followed_id.eql?(follower_id) 
+    end
+  end
 end
